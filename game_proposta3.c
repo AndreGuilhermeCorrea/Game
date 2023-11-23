b09175873cb7295f221f5f310951203101790fbc
AndreGuilhermeCorrea <de_correa@msn.com>
2023-11-23 10:24:35 -0300

    Atualizando e comentando a proposta 3

--- game_proposta3.c
+++ game_proposta3.c
@@ -1,36 +1,71 @@
+/*Este é um jogo de console simples, onde você controla um personagem (P)
+em uma caverna representada por uma grade.
+Seu objetivo é chegar até a saída (  ) evitando os monstros (M)
+que se move aleatoriamente pela caverna.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h> // Inclusão da biblioteca para não precisar apertar ENTER.
 
-// tamanho da caverna pré estabelecida
+// Inclusão da biblioteca para não precisar apertar ENTER.
+#include <conio.h> 
+
+//tamanho da caverna pré estabelecida
 #define MAX_LINHA 20
 #define MAX_COLUNA 30
+
+//define a quantidade de monstros no jogo
 #define NUM_MONSTROS 5
 
 int main()
 {
+
+  // Inicialização das variáveis
+
+  //Gerador de números aleatórios diferentes em momentos diferentes
   srand(time(NULL));
-  int ox = MAX_COLUNA / 2, oy = MAX_LINHA - 2;
-  int px = 1, py = 1;
+  
+  //Posição da saída do jogador
+  int ox = MAX_COLUNA / 2;
+  
+  int oy = MAX_LINHA - 2;
+
+  //Posição inicial do jogador.
+  int px = 1;
+
+  int py = 1;
+
+  //Posições dos monstros
   int monstros[NUM_MONSTROS][2];
-  // Inicializa as posições dos monstros aleatoriamente
+  
+  //Inicializa as posições dos monstros aleatoriamente
   for (int i = 0; i < NUM_MONSTROS; i++)
   {
     monstros[i][0] = rand() % MAX_COLUNA;
     monstros[i][1] = rand() % MAX_LINHA;
   }
 
+
+  //Variáveis de iteração para percorrer a grade
   int x, y;
+  
+  //Variável que armazena o comando digitado pelo jogador.
   char comando;
+
+  //Variável que controla se o movimento do jogador foi realizado.
   int movimentoRealizado;
+
+  //Variável que estabelece o nível de dificuldade inicial do jogo.
   int dificuldade = 1;
 
+  // Loop Principal:
   for (;;)
   {
+    
+    //O loop será interrompido quando o jogador atingir a saída ou colidir com o monstro.
     system("CLS");
 
-    // imprime a caverna
+    //Imprime a caverna
     for (y = 0; y < MAX_LINHA; y++)
     {
       for (x = 0; x < MAX_COLUNA; x++)
@@ -40,11 +75,11 @@ int main()
           if (y == MAX_LINHA - 1 && x >= (MAX_COLUNA / 2 - 1) && x <= (MAX_COLUNA / 2))
           {
             printf("  "); // Dois espaços vazios no meio da parede
-            x++;          // Avança mais uma posição para compensar o segundo espaço
+            x++;//Avança mais uma posição para compensar o segundo espaço
           }
           else
           {
-            printf("#");
+            printf("X");
           }
         }
         else if (x == px && y == py)
@@ -65,7 +100,7 @@ int main()
           }
           if (!ehMonstro)
           {
-            printf(".");
+            printf(" ");
           }
         }
       }
@@ -74,11 +109,11 @@ int main()
 
     printf("\nDificuldade: %d\n", dificuldade);
 
-    // Recebe o comando do jogador
+    //Recebe o comando do jogador
     printf("Comando: C (cima)\tD (direita)\tB (baixo)\tE (esquerda)\tX (encerrar)\n");
     printf("Sua vez! Digite um comando: ");
     comando = _getch(); // Substitui Scanf para reconhecer a tecla assim que digitada.
-
+    //Recebe o movimento do Jogador.
     movimentoRealizado = 1;
     switch (comando)
     {
@@ -117,7 +152,8 @@ int main()
         movimentoRealizado = 0;
       }
       break;
-    // botão "x" para encerrar o game
+
+    // botão "x" para encerrar o game.
     case 'x':
       printf("\n\nJogo encerrado pelo jogador.\n");
       return 0;
@@ -128,25 +164,25 @@ int main()
       printf("Pressione uma tecla para continuar!\n");
       system("PAUSE");
     }
-
+    //Movimento Aleatório do Monstro.
     if (movimentoRealizado == 1)
     {
 
-      // Fator de escala para diminuir a velocidade dos monstros
+      // Fator de escala para diminuir a velocidade dos monstros.
       float fatorEscala = 0.5;
 
-      // Movimenta os monstros aleatoriamente
-      // Atualiza a posição dos monstros para perseguir o jogador
+      // Movimenta os monstros aleatoriamente.
+      // Atualiza a posição dos monstros para perseguir o jogador.
       for (int i = 0; i < NUM_MONSTROS; i++)
       {
         int deltaX = px - monstros[i][0];
         int deltaY = py - monstros[i][1];
 
-        // Move o monstro na direção do jogador com velocidade reduzida
+        //Move o monstro na direção do jogador com velocidade reduzida.
         monstros[i][0] += (int)(fatorEscala * deltaX);
         monstros[i][1] += (int)(fatorEscala * deltaY);
 
-        // Verifica limites da caverna
+        //Verifica limites da caverna.
         if (monstros[i][0] < 1)
         {
           monstros[i][0] = 1;
@@ -165,12 +201,12 @@ int main()
           monstros[i][1] = MAX_LINHA - 2;
         }
 
-        // Verifica se dois monstros estão na mesma posição e ajusta
+        //Verifica se dois monstros estão na mesma posição e ajusta.
         for (int j = 0; j < NUM_MONSTROS; j++)
         {
           if (i != j && monstros[i][0] == monstros[j][0] && monstros[i][1] == monstros[j][1])
           {
-            // Se encontrou duas posições iguais, ajusta a posição do monstro i
+            //Se encontrou duas posições iguais, ajusta a posição do monstro.
             monstros[i][0] = rand() % MAX_COLUNA;
             monstros[i][1] = rand() % MAX_LINHA;
           }
@@ -178,7 +214,9 @@ int main()
       }
     }
 
-    // Verifica condições de vitória e derrota
+    //Verifica condições de vitória e derrota.
+    
+    // Verifica se o jogador atingiu a saída (YOU WIN).
     if ((px == ox && py == oy) || (px == ox + 1 && py == oy) || (px == ox - 1 && py == oy))
     {
       printf("\n\nYOU WIN\n");
@@ -199,6 +237,7 @@ int main()
     {
       for (int i = 0; i < NUM_MONSTROS; i++)
       {
+        //Verifica se o jogador colidiu com o monstro (GAME OVER).
         if (px == monstros[i][0] && py == monstros[i][1])
         {
           printf("\n\nGAME OVER\n");
@@ -207,7 +246,7 @@ int main()
       }
     }
   }
-
+  //Finalização do Programa.
   system("PAUSE");
   return 0;
 }
